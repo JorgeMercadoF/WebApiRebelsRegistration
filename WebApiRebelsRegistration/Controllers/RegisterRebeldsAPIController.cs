3cffcb0e9db172924320b2ffbd92074a18bd7dd3
JorgeMercadoF <47115493+JorgeMercadoF@users.noreply.github.com>
2019-02-13 18:40:59 +0100

    Añadido controllerAPI

---
+++ WebApiRebelsRegistration/Controllers/RegisterRebeldsAPIController.cs
@@ -0,0 +1,115 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiRebelsRegistration.Models;
+
+namespace WebApiRebelsRegistration.Controllers
+{
+    public class RegisterRebeldsAPIController : ApiController
+    {
+        private JorgeMercadoEntities db = new JorgeMercadoEntities();
+
+        // GET: api/RegisterRebeldsAPI
+        public IQueryable<RegisterRebelds> GetRegisterRebelds()
+        {
+            return db.RegisterRebelds;
+        }
+
+        // GET: api/RegisterRebeldsAPI/5
+        [ResponseType(typeof(RegisterRebelds))]
+        public async Task<IHttpActionResult> GetRegisterRebelds(int id)
+        {
+            RegisterRebelds registerRebelds = await db.RegisterRebelds.FindAsync(id);
+            if (registerRebelds == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(registerRebelds);
+        }
+
+        // PUT: api/RegisterRebeldsAPI/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutRegisterRebelds(int id, RegisterRebelds registerRebelds)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != registerRebelds.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(registerRebelds).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegisterRebeldsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/RegisterRebeldsAPI
+        [ResponseType(typeof(RegisterRebelds))]
+        public async Task<IHttpActionResult> PostRegisterRebelds(RegisterRebelds registerRebelds)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.RegisterRebelds.Add(registerRebelds);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = registerRebelds.Id }, registerRebelds);
+        }
+
+        // DELETE: api/RegisterRebeldsAPI/5
+        [ResponseType(typeof(RegisterRebelds))]
+        public async Task<IHttpActionResult> DeleteRegisterRebelds(int id)
+        {
+            RegisterRebelds registerRebelds = await db.RegisterRebelds.FindAsync(id);
+            if (registerRebelds == null)
+            {
+                return NotFound();
+            }
+
+            db.RegisterRebelds.Remove(registerRebelds);
+            await db.SaveChangesAsync();
+
+            return Ok(registerRebelds);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RegisterRebeldsExists(int id)
+        {
+            return db.RegisterRebelds.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- WebApiRebelsRegistration/Models/MetaData/RegisterRebeldsMetadata.cs
+++ WebApiRebelsRegistration/Models/MetaData/RegisterRebeldsMetadata.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiRebelsRegistration.Models
 {
@@ -12,13 +13,20 @@ namespace WebApiRebelsRegistration.Models
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Es necesario informar el Nombre del Rebelde")]
+        [DisplayName("Nombre del Rebelde")]
         public int IdRebeld { get; set; }
         [Required(ErrorMessage = "Es necesario informar el Planeta del Rebelde")]
+        [DisplayName("Nombre del Planeta")]
         public int IdPlanet { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         [Required(ErrorMessage = "Es necesario informar la Fecha de captura")]
+        [DisplayName("Fecha de captura")]
         public System.DateTime DateRegsiter { get; set; }
 
-        //public virtual Planets Planets { get; set; }
-        //public virtual Rebelds Rebelds { get; set; }
+        [DisplayName("Planeta")]
+        public virtual Planets Planets { get; set; }
+        [DisplayName("Rebelde")]
+        public virtual Rebelds Rebelds { get; set; }
     }
 }
